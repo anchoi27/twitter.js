10a423c361c9c2fa08cc82a3c192173057984dd6
yuyingliu331 <liuyuying90@live.cn>
2016-08-02 15:46:54 -0400

    added the path for '/special'

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 var express = require( 'express' );
 var app = express();
 
+app.listen(3000, function(){
+  console.log("server listening");
+});
+
+app.use(function (request, response, next) {
+    console.log("This request\'s method:"+request.method);
+    next();
+});
+
 app.get('/', function(request, response) {
   response.send("Welcome!");
 });
 
-app.listen(3000, function(){
-  console.log("server listening");
+app.get('/news', function(request, response) {
+  response.send("This is the news page!");
+});
+
+app.use('/special/', function (request, response, next) {
+    console.log("this is in the special area's sub folders!");
+    console.log("Special //This request\'s method: "+request.method);
+    next();
+});
+
+app.get('/special', function(request, response,next) {
+  response.send("you reached the special area.");
+});
+
+app.get('/special/subpath', function(request, response,next) {
+  response.send("you reached the special area. - subpath");
 });
