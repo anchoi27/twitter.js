18c45e573cab302c34ce463051951758a316ca46
yuyingliu331 <liuyuying90@live.cn>
2016-08-02 18:33:43 -0400

    updated tweetBank.js

--- tweetBank.js
+++ tweetBank.js
@@ -21,21 +21,32 @@ var randArrayEl = function(arr) {
 
 module.exports = { add: add, list: list, find: find };
 
-var getFakeName = function() {
-  var fakeFirsts = ['Nimit', 'Dave', 'Shanna', 'Charlotte', 'Scott', 'Ayana', 'Omri', 'Gabriel', 'Joe'];
-  var fakeLasts = ['Hashington', 'Stackson', 'McQueue', 'OLogn', 'Ternary', 'Claujure', 'Dunderproto', 'Binder', 'Docsreader', 'Ecma'];
-  return randArrayEl(fakeFirsts) + " " + randArrayEl(fakeLasts);
-};
+// var getFakeName = function() {
+var fakeFirsts = ['Nimit', 'Dave', 'Shanna', 'Charlotte', 'Scott', 'Ayana', 'Omri', 'Gabriel', 'Joe'];
+var fakeLasts = ['Hashington', 'Stackson', 'McQueue', 'OLogn', 'Ternary', 'Claujure', 'Dunderproto', 'Binder', 'Docsreader', 'Ecma'];
+var awesome_adj = ['awesome', 'breathtaking', 'amazing', 'funny', 'sweet', 'cool', 'wonderful', 'mindblowing'];
+//   return randArrayEl(fakeFirsts) + " " + randArrayEl(fakeLasts);
+// };
 
-var getFakeTweet = function() {
-  var awesome_adj = ['awesome', 'breathtaking', 'amazing', 'funny', 'sweet', 'cool', 'wonderful', 'mindblowing'];
-  return "Fullstack Academy is " + randArrayEl(awesome_adj) + "! The instructors are just so " + randArrayEl(awesome_adj) + ". #fullstacklove #codedreams";
-};
+// var getFakeTweet = function() {
+//   var awesome_adj = ['awesome', 'breathtaking', 'amazing', 'funny', 'sweet', 'cool', 'wonderful', 'mindblowing'];
+//   return "Fullstack Academy is " + randArrayEl(awesome_adj) + "! The instructors are just so " + randArrayEl(awesome_adj) + ". #fullstacklove #codedreams";
+// };
+
+for (var k = 0; k < 3; k++) {
+  var theName = fakeFirsts[k] + " " + fakeLasts[k];
+  var theTweet = "Grace Hopper Academy is so much better. duh! " + awesome_adj[k];
+  add(theName, theTweet);
+}
+
+console.log(data);
+/*
 
 for (var i = 0; i < 5; i++) {
   module.exports.add(getFakeName(), getFakeTweet());
 }
 
+*/
 
 
 
